e39f3cd271852cd70a5ccb8196fbd4c7053f1a65
Ely Gandara <ely.gandara.c@gmail.com>
2020-02-12 08:45:54 -0700

    Hello World implemented

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+//palabra reservada, using estara trabajando con el entorno de system
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            
+            CultureInfo uiCulture1 = CultureInfo.CurrentUICulture;
+            string myLanguage = uiCulture1.TwoLetterISOLanguageName.ToString();
+            string test = "en";
+                
+            if (myLanguage == test)
+            {
+                Console.WriteLine("Hello World");
+            }
+            else 
+            {
+                Console.WriteLine("Hello, Hola, Konnichiwa");   
+            }
+
+        }
+    }
+}
